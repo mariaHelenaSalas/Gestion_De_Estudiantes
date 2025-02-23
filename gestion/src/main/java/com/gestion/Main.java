122e7e0ab628e9900ad3c573105fca0164830f17
maría Helena Salas <mariahelenasalas83@gmail.com>
2025-02-22 21:59:13 -0500

    ejercicio terminado

--- gestion/src/main/java/com/gestion/Main.java
+++ gestion/src/main/java/com/gestion/Main.java
@@ -1,43 +1,49 @@
 package com.gestion;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
-        Scanner teclado = new Scanner(System.in);
-        List<String>estudiantes = new ArrayList<>();
 
+        Scanner teclado = new Scanner(System.in);
+        List<String> estudiantes = new ArrayList<>();
 
         System.out.println("Ingresar la cantidad de estudiante que desea registrar: ");
         int cantidad = teclado.nextInt();
-        teclado.nextInt();
-
-        //paso ahora aregistrar los nombres utilizo for
+        teclado.nextLine();
+        // paso ahora aregistrar los nombres utilizo for
 
         for (int i = 0; i < cantidad; i++) {
-            System.out.println("Ingrese el nombre dl estudiante: " + (i + 1)  );
+            System.out.println("Ingrese el nombre dl estudiante # " + (i + 1));
             String nombre = teclado.nextLine();
             estudiantes.add(nombre);
-            
+            System.out.println("==========================================");
+
         }
 
-       System.out.println( "\n Lista complta de estudiante");
-       for (String estudiante : estudiantes){
-        System.out.println(estudiante);
-       }
+        System.out.println("\n *****    Lista complta de estudiante  *****");
+        for (String estudiante : estudiantes) {
+            System.out.println(estudiante);
+        }
 
-        
+        Collections.sort(estudiantes);
+        System.out.println("\n *****    Lista de estudiante ordenada   *****");
+        for (String estudiante : estudiantes) {
+            System.out.println(estudiante);
+        }
 
+       System.out.println("\n *****   ingrese el estudiante a buscar:   *****");
+         String buscar = teclado.nextLine();
+            if (estudiantes.contains(buscar)) {
+                System.out.println("El estudiante si se encuentra en la lista");
+            } else {
+                System.out.println("El estudiante no se encuentra en la lista");
+            }
 
-
-
-
-
-
-
+            teclado.close();
 
 
     }
