e9086c7f7da69897bc7c0b6ce3ff9c24aae10a46
maría Helena Salas <mariahelenasalas83@gmail.com>
2025-02-22 23:32:58 -0500

    imagen de ejemplo

--- gestion/src/main/java/com/gestion/Main.java
+++ gestion/src/main/java/com/gestion/Main.java
@@ -17,26 +17,27 @@ public class Main {
         // paso ahora aregistrar los nombres utilizo for
 
         for (int i = 0; i < cantidad; i++) {
-            System.out.println("Ingrese el nombre dl estudiante # " + (i + 1));
+            System.out.println("Ingrese el nombre del estudiante # " + (i + 1));
             String nombre = teclado.nextLine();
             estudiantes.add(nombre);
             System.out.println("==========================================");
 
         }
 
-        System.out.println("\n *****    Lista complta de estudiante  *****");
+        System.out.println("\n *****    Lista complta de estudiantes  *****");
         for (String estudiante : estudiantes) {
             System.out.println(estudiante);
         }
 
         Collections.sort(estudiantes);
-        System.out.println("\n *****    Lista de estudiante ordenada   *****");
+        System.out.println("\n *****    Lista de estudiantes ordenada   *****");
         for (String estudiante : estudiantes) {
             System.out.println(estudiante);
         }
 
-       System.out.println("\n *****   ingrese el estudiante a buscar:   *****");
+       System.out.println("\n *****   ingrese el estudiante a buscar   *****");
          String buscar = teclado.nextLine();
+         
             if (estudiantes.contains(buscar)) {
                 System.out.println("El estudiante si se encuentra en la lista");
             } else {
